482cd686d6f4243526946067d8743738532db695
pvsukalkar <pvsukalkar@iitj.ac.in>
2013-09-19 23:00:28 +0530

    Create chessFunc1.h

---
+++ chessFunc1.h
@@ -0,0 +1,12 @@
+#include "chessFunc.h"
+
+//check for castling
+void castling(int b){
+     //castling of black king
+     if(b==1){
+                 
+     }
+     if(b==0){
+              
+     }
+}
