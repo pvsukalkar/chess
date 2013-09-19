ad89c4e323a41ae983603577201351d3b4f6b6e9
pvsukalkar <pvsukalkar@iitj.ac.in>
2013-09-19 22:57:23 +0530

    Create color.cpp

---
+++ color.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <windows.h>
+#include <iomanip>
+
+using namespace std;
+
+enum colour { DARKBLUE = 1, DARKGREEN, DARKTEAL, DARKRED, DARKPINK, DARKYELLOW, GRAY, DARKGRAY, BLUE, GREEN, TEAL, RED, PINK, YELLOW, WHITE };
+
+struct setcolour
+{
+   colour _c;
+   HANDLE _console_handle;
+
+
+       setcolour(colour c, HANDLE console_handle)
+           : _c(c), _console_handle(0)
+       { 
+           _console_handle = console_handle;
+       }
+};
+
+// We could use a template here, making it more generic. Wide streams won't
+// work with this version.
+basic_ostream<char> &operator<<(basic_ostream<char> &s, const setcolour &ref)
+{
+    SetConsoleTextAttribute(ref._console_handle, ref._c);
+    return s;
+}
+
